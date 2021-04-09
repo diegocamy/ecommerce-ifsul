7f0815f0ef20bbf3cfd447798c4004e30e85a96e
dcamy <diegocamyy@gmail.com>
2021-04-09 12:08:42 -0300

    Fue agregado react router y sus rutas correspondientes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home</h1>} />
+        <Route path="/category" render={() => <h1>category</h1>} />
+        <Route path="/product" render={() => <h1>product</h1>} />
+        <Route render={() => <h1>404 not found</h1>} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>NAVBAR</h3>
+    </nav>
+  );
+};
+
+export default Navbar;
